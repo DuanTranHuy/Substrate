38a17bd4fea6e27fa8710db8415025c81a32006f
Trần Huy Duẫn <duanthse03268@gmail.com>
2022-04-07 10:06:59 +0700

    Update chain_spec.rs

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -162,8 +162,8 @@ fn testnet_genesis(
 			code: wasm_binary.to_vec(),
 		},
 		balances: BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
+			// Configure endowed accounts with initial balance of 1 << 80.
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 80)).collect(),
 		},
 		aura: AuraConfig {
 			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
